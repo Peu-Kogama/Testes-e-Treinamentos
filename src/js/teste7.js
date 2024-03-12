cda07b599dcd906c8938700759a05feb88425f52
Peu-Kogama <133553487+Peu-Kogama@users.noreply.github.com>
2024-03-11 21:17:31 -0300

    Add files via upload

---
+++ src/js/teste7.js
@@ -0,0 +1,15 @@
+// document.getElementById("radio").checked = true;
+
+// setInterval( function(){
+
+// }, 2000)
+
+// function nextImage(){
+//     count++;
+//     if(count > 4){
+//         count = 1;
+//     }
+
+//     document.getElementById("radio"+count).checked = true
+// }
+
